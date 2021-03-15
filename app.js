4438b25e9df3d0c9bcdfcb1b3a3141d24b5993cd
christiaanbraun <christiaan.braun@hva.nl>
2021-03-15 15:01:50 +0100

    Change databasename in connection string

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv/config');
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 // Get .env Variables
 const hostURL = process.env.URL;
 const hostPort = process.env.PORT || 3000;
-const dbConnection = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@buster.boqc9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const dbConnection = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@buster.boqc9.mongodb.net/teamfeature?retryWrites=true&w=majority`;
 
 // Import Routes
-const homeRoutes = require('./routes/homeRoutes');
+const homeRoutes = require("./routes/homeRoutes");
 
 // Set Templating Engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Set Public Directory as a Static Directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
@@ -23,16 +23,16 @@ app.use(express.json());
 
 // Connect to Database
 mongoose.connect(
-	dbConnection,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: true,
-	},
-	() => {
-		console.log('Succesfully Connected to Database');
-	}
+  dbConnection,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  },
+  () => {
+    console.log("Succesfully Connected to Database");
+  }
 );
 
 // Route Middlewares
@@ -40,10 +40,10 @@ app.use(homeRoutes);
 
 // Show 404 Page if Page Doesn't Exists
 app.use((req, res, next) => {
-	res.status(404).send('This page does not exist!');
+  res.status(404).send("This page does not exist!");
 });
 
 // Start Server at Given Host and Port
 app.listen(hostPort, () => {
-	console.log(`App Listening at ${hostURL}:${hostPort}`);
+  console.log(`App Listening at ${hostURL}:${hostPort}`);
 });
