ccf7f7b954f67e4f6ea75643516968ed124a9e8a
Patrick van der Bijl <pmvdbijl7@gmail.com>
2021-03-16 12:53:02 +0100

    Update genre model

--- models/Genre.js
+++ models/Genre.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 
 const genreSchema = new mongoose.Schema({
 	name: String,
+	movies: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Movie',
+		},
+	],
+	series: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Series',
+		},
+	],
 });
 
 module.exports = mongoose.model('Genre', genreSchema);
