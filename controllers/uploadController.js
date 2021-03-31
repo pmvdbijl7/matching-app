c8a2895067e1f08e36a773bc3cf67f0a00646e12
Patrick van der Bijl <pmvdbijl7@gmail.com>
2021-03-31 22:14:32 +0200

    Update packages

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,11 +1,13 @@
 const multer = require('multer');
 const path = require('path');
 
-// Set Uplaods Storage Engine
+// Set Uploads Storage Engine
 const uploadStorage = multer.diskStorage({
+	// Determine uploads save
 	destination: (req, file, cb) => {
 		cb(null, 'public/uploads');
 	},
+	// Determine file name
 	filename: (req, file, cb) => {
 		cb(
 			null,
